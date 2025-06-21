bf7fb0291faaeaa6cf05754b60a4dc21bbf6e48a
maropce <47991391+maropce@users.noreply.github.com>
2025-06-21 17:07:22 +0200

    Add files via upload

---
+++ projekt2/ObjLoader.cs
@@ -0,0 +1,110 @@
+﻿using OpenTK.Graphics.OpenGL4;
+using GlmSharp;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Globalization;
+
+namespace PMLabs
+{
+    public class Model
+    {
+        int vao, vbo, ebo, count;
+        public vec3[] V; public vec3[] N; public vec2[] UV; public int[] E;
+        public void SetupGL()
+        {
+            vao = GL.GenVertexArray(); vbo = GL.GenBuffer(); ebo = GL.GenBuffer();
+            GL.BindVertexArray(vao);
+            int vcount = V.Length;
+            float[] buf = new float[vcount * 8];
+            for (int i = 0; i < vcount; i++)
+            {
+                buf[i * 8 + 0] = V[i].x; buf[i * 8 + 1] = V[i].y; buf[i * 8 + 2] = V[i].z;
+                buf[i * 8 + 3] = N[i].x; buf[i * 8 + 4] = N[i].y; buf[i * 8 + 5] = N[i].z;
+                buf[i * 8 + 6] = UV[i].x; buf[i * 8 + 7] = UV[i].y;
+            }
+            GL.BindBuffer(BufferTarget.ArrayBuffer, vbo);
+            GL.BufferData(BufferTarget.ArrayBuffer, buf.Length * 4, buf, BufferUsageHint.StaticDraw);
+            GL.BindBuffer(BufferTarget.ElementArrayBuffer, ebo);
+            GL.BufferData(BufferTarget.ElementArrayBuffer, E.Length * 4, E, BufferUsageHint.StaticDraw);
+            int stride = 8 * 4;
+            GL.EnableVertexAttribArray(0); GL.VertexAttribPointer(0, 3, VertexAttribPointerType.Float, false, stride, 0);
+            GL.EnableVertexAttribArray(1); GL.VertexAttribPointer(1, 3, VertexAttribPointerType.Float, false, stride, 3 * 4);
+            GL.EnableVertexAttribArray(2); GL.VertexAttribPointer(2, 2, VertexAttribPointerType.Float, false, stride, 6 * 4);
+            GL.BindVertexArray(0);
+            count = E.Length;
+        }
+        public void Draw() { GL.BindVertexArray(vao); GL.DrawElements(PrimitiveType.Triangles, count, DrawElementsType.UnsignedInt, 0); }
+    }
+
+    public static class ObjLoader
+    {
+        public static Model Load(string file)
+        {
+            var V = new List<vec3>(); var N = new List<vec3>(); var UV = new List<vec2>();
+            var vI = new List<int>(); var vtI = new List<int>(); var vnI = new List<int>();
+
+            foreach (var l in File.ReadAllLines(file))
+            {
+                if (string.IsNullOrWhiteSpace(l)) continue;
+                if (l.StartsWith("#")) continue;
+
+                var s = l.Split(new[] { ' ' }, StringSplitOptions.RemoveEmptyEntries);
+                if (s.Length == 0) continue;
+
+                if (s[0] == "v" && s.Length >= 4)
+                    V.Add(new vec3(
+                        float.Parse(s[1], CultureInfo.InvariantCulture),
+                        float.Parse(s[2], CultureInfo.InvariantCulture),
+                        float.Parse(s[3], CultureInfo.InvariantCulture)));
+
+                else if (s[0] == "vn" && s.Length >= 4)
+                    N.Add(new vec3(
+                        float.Parse(s[1], CultureInfo.InvariantCulture),
+                        float.Parse(s[2], CultureInfo.InvariantCulture),
+                        float.Parse(s[3], CultureInfo.InvariantCulture)));
+
+                else if (s[0] == "vt" && s.Length >= 3)
+                    UV.Add(new vec2(
+                        float.Parse(s[1], CultureInfo.InvariantCulture),
+                        float.Parse(s[2], CultureInfo.InvariantCulture)));
+
+                else if (s[0] == "f" && s.Length >= 4)
+                {
+                    for (int i = 1; i <= 3; i++)
+                    {
+                        var parts = s[i].Split('/');
+                        if (parts.Length >= 3)
+                        {
+                            vI.Add(int.Parse(parts[0]) - 1);
+                            vtI.Add(int.Parse(parts[1]) - 1);
+                            vnI.Add(int.Parse(parts[2]) - 1);
+                        }
+                    }
+                }
+            }
+
+            var m = new Model();
+            int n = vI.Count;
+            m.V = new vec3[n]; m.UV = new vec2[n]; m.N = new vec3[n];
+            m.E = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                m.V[i] = V[vI[i]];
+                if (vtI[i] >= 0 && vtI[i] < UV.Count)
+                    m.UV[i] = UV[vtI[i]];
+                else
+                    m.UV[i] = new vec2(0, 0);
+
+                if (vnI[i] >= 0 && vnI[i] < N.Count)
+                    m.N[i] = N[vnI[i]];
+                else
+                    m.N[i] = new vec3(0, 0, 1);
+
+                m.E[i] = i;
+            }
+
+            return m;
+        }
+    }
+}
---
+++ projekt2/Program.cs
@@ -0,0 +1,151 @@
+﻿using GLFW;
+using GlmSharp;
+using OpenTK;
+using OpenTK.Graphics.OpenGL4;
+using Shaders;
+using System;
+using System.Drawing;
+
+namespace PMLabs
+{
+    public class BC : IBindingsContext
+    {
+        public IntPtr GetProcAddress(string procName) =>
+            Glfw.GetProcAddress(procName);
+    }
+
+    class Program
+    {
+        static ShaderProgram shader;
+        static Model model;
+        static float rotX, rotY;
+        static KeyCallback kc = KeyProcessor;
+
+        public static void KeyProcessor(IntPtr w, Keys key, int sc, InputState st, ModifierKeys mk)
+        {
+            if (st == InputState.Press || st == InputState.Repeat)
+            {
+                switch (key)
+                {
+                    case Keys.Up: rotX -= 5; break;
+                    case Keys.Down: rotX += 5; break;
+                    case Keys.Left: rotY -= 5; break;
+                    case Keys.Right: rotY += 5; break;
+                }
+            }
+        }
+
+        public static int ReadTexture(string fn, TextureUnit tu = TextureUnit.Texture0)
+        {
+            int tex = GL.GenTexture();
+            GL.ActiveTexture(tu);
+            GL.BindTexture(TextureTarget.Texture2D, tex);
+            Bitmap bmp = new Bitmap(fn);
+            var data = bmp.LockBits(new Rectangle(0, 0, bmp.Width, bmp.Height),
+                System.Drawing.Imaging.ImageLockMode.ReadOnly,
+                System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+            GL.TexImage2D(TextureTarget.Texture2D, 0, PixelInternalFormat.Rgba,
+                          data.Width, data.Height, 0,
+                          PixelFormat.Bgra, PixelType.UnsignedByte, data.Scan0);
+            bmp.UnlockBits(data); bmp.Dispose();
+            GL.TexParameter(TextureTarget.Texture2D,
+                TextureParameterName.TextureMagFilter, (int)TextureMagFilter.Linear);
+            GL.TexParameter(TextureTarget.Texture2D,
+                TextureParameterName.TextureMinFilter, (int)TextureMinFilter.Linear);
+            return tex;
+        }
+
+        static void Init(Window win)
+        {
+            GL.ClearColor(0, 0, 0, 1);
+            shader = new ShaderProgram("v_shader.glsl", "f_shader.glsl");
+            Glfw.SetKeyCallback(win, kc);
+            GL.Enable(EnableCap.DepthTest);
+
+            model = ObjLoader.Load("../../../M.obj");
+            model.SetupGL();
+
+            vec3 centroid = new vec3(0, 0, 0);
+            int count = model.V.Length;
+
+            for (int i = 0; i < count; i++)
+            {
+                centroid += model.V[i];
+            }
+            centroid /= count;
+
+            for (int i = 0; i < count; i++)
+            {
+                model.V[i] -= centroid;
+            }
+
+            model.SetupGL();
+
+
+            int t = ReadTexture("../../../marble.jpg", TextureUnit.Texture0);
+            shader.Use();
+            GL.Uniform1(shader.U("tex0"), 0);
+        }
+
+        static void Draw(Window win)
+        {
+            GL.Clear(ClearBufferMask.ColorBufferBit | ClearBufferMask.DepthBufferBit);
+            float w = 800, h = 600;
+            var P = mat4.Perspective(glm.Radians(50f), w / h, 0.1f, 100f);
+            var V = mat4.LookAt(new vec3(0, 1, 5), new vec3(0, 0, 0), new vec3(0, 1, 0));
+
+            float scale = 0.02f;
+            var M = mat4.Scale(new vec3(scale)) * mat4.RotateX(glm.Radians(rotX)) * mat4.RotateY(glm.Radians(rotY));
+
+            shader.Use();
+            GL.UniformMatrix4(shader.U("P"), 1, false, P.Values1D);
+            GL.UniformMatrix4(shader.U("V"), 1, false, V.Values1D);
+            GL.UniformMatrix4(shader.U("M"), 1, false, M.Values1D);
+
+           
+            float t = (float)Glfw.Time;
+            vec3 c1 = new vec3(0.078f, 0.118f, 0.314f);  // #141e50
+            vec3 c2 = new vec3(0.0f, 0.423f, 0.980f);  // #006cfa
+            vec3 c3 = new vec3(0.137f, 0.741f, 1.0f);    // #23bdff
+
+            float phase = (t % 6f) / 2f;
+            vec3 clr;
+
+            if (phase < 1f)
+                clr = (1 - phase) * c1 + phase * c2;
+            else if (phase < 2f)
+                clr = (2 - phase) * c2 + (phase - 1f) * c3;
+            else
+                clr = (3 - phase) * c3 + (phase - 2f) * c1;
+
+            GL.Uniform3(shader.U("lightColor"), clr.x, clr.y, clr.z);
+
+            vec3 lightPos = new vec3(5, 5, 5);
+            GL.Uniform3(shader.U("lightPos"), lightPos.x, lightPos.y, lightPos.z);
+
+
+            model.Draw();
+            Glfw.SwapBuffers(win);
+        }
+
+        static void Main()
+        {
+            Glfw.Init();
+            var win = Glfw.CreateWindow(800, 600, "Litera M", GLFW.Monitor.None, Window.None);
+            Glfw.MakeContextCurrent(win);
+            Glfw.SwapInterval(1);
+
+            GL.LoadBindings(new BC());
+            Init(win);
+
+            Glfw.Time = 0;
+            while (!Glfw.WindowShouldClose(win))
+            {
+                Draw(win);
+                Glfw.PollEvents();
+            }
+
+            Glfw.Terminate();
+        }
+    }
+}
